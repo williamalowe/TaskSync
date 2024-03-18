0074ac7e2c3a8c8683f34420cb87abbe47ca1132
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-03-18 15:28:17 +1100

    feat: add welcome-view component

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -1,9 +1,17 @@
-import './Welcome.module.css'
+import { NavLink } from 'react-router-dom'
+import styles from './Welcome.module.css'
 
 function Welcome() {
   return (
-    <main>
-      Hello World
+    <main className={styles.welcome}>
+      <div className={styles.content}>
+        <h1>Welcome to <span className={styles.title}>TaskSync</span>.</h1>
+        <h3>
+          Click 
+          <NavLink> here </NavLink>
+          to start working.
+        </h3>
+      </div>
     </main>
   )
 }
